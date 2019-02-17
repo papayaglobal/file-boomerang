35642af6c1865d962be4fab3bebc54eaa42b4ef4
amaziagur <amazia@payayaglobal.com>
2019-02-17 15:15:11 +0200

    push to jcenter

---
+++ .circleci/destribute_to_jcenter.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -e
+# more bash-friendly output for jq
+JQ="jq --raw-output --exit-status"
+
+distribute(){
+    if [ "${CIRCLE_BRANCH}" == "master" ]; then
+        mvn -s settings.xml deploy -Dbintray.user=$BINTRAY_USER -Dbintray.key=$BINTRAY_KEY
+        echo "file-boomerang was deployed to jcenter"
+    fi
+}
+
+distribute
